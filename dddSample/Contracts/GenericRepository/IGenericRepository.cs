6021a8ca7e9d2e9edd0becc0d819d76faa49ba46
Lori <lorikmanaj@hotmail.com>
2023-07-25 15:28:40 +0200

    Added IGeneric Repo interface

---
+++ dddSample/Contracts/GenericRepository/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.GenericRepository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetAsync(int? id);
+        Task<List<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        Task DeleteAsync(int id);
+        Task UpdateAsync(T entity);
+        Task<bool> Exists(int id);
+    }
+}
