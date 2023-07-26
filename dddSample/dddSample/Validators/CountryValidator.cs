cc5f3b181ee0c938f265de8ceb3a221d2b84e521
Lori <lorikmanaj@hotmail.com>
2023-07-26 13:45:00 +0200

    Added CountryValidator (must implement in backend)

---
+++ dddSample/dddSample/Validators/CountryValidator.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Models;
+using FluentValidation;
+
+namespace dddSample.Validators
+{
+    public class CountryValidator : AbstractValidator<Country>
+    {
+        public CountryValidator()
+        {
+            RuleFor(country => country.Name).NotEmpty().MinimumLength(4).MaximumLength(30);
+            RuleFor(country => country.ShortName).NotEmpty().MinimumLength(2).MaximumLength(4);
+        }
+    }
+}
