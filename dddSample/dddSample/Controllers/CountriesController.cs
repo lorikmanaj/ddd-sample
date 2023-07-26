2ae45b64c49a00c59a3e3f2f5608100049b412f8
Lori <lorikmanaj@hotmail.com>
2023-07-26 13:39:00 +0200

    Added Countries controller with CRUD operations (FluentValidation to be implemented)

---
+++ dddSample/dddSample/Controllers/CountriesController.cs
@@ -0,0 +1,76 @@
+﻿using Core.Exceptions;
+using Core.Interfaces;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace dddSample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        private readonly ICountriesRepository _countriesRepository;
+
+        public CountriesController(ICountriesRepository countriesRepository)
+        {
+            _countriesRepository = countriesRepository;
+        }
+
+        // GET: api/Countries
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Hotel>>> GetCountries()
+        {
+            var countries = await _countriesRepository.GetAllAsync();
+            return countries == null ? throw new BadRequestException("Countries failed to load.") : Ok(countries);
+        }
+
+        // GET: api/Countries/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Hotel>> GetCountry([FromRoute] int id)
+        {
+            var country = await _countriesRepository.GetAsync(id);
+            return country == null ? throw new NotFoundException("Country not found.", id) : Ok(country);
+        }
+
+        // PUT: api/Countries/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCountry([FromRoute] int id, [FromBody] Country country)
+        {
+            if (!ModelState.IsValid)
+                throw new BadRequestException("Invalid request data.");
+
+            if (id != country.Id)
+                return BadRequest(new BadRequestException("Country ID missmatch"));
+
+            try
+            {
+                await _countriesRepository.UpdateAsync(country);
+                return Ok(country);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!await _countriesRepository.Exists(id))
+                    throw new NotFoundException("Country not found.", id);
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Countries/
+        [HttpPost]
+        public async Task<IActionResult> PostCountry([FromBody] Country country)
+        {
+            var result = await _countriesRepository.AddAsync(country);
+            return CreatedAtAction(nameof(GetCountry), new { id = result.Id }, result);
+        }
+
+        // DELETE: api/Countries/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCountry([FromRoute] int id)
+        {
+            await _countriesRepository.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
