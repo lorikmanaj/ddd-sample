e0d487caeaa457905f111af2de74a35ebfbfaabc
Lori <lorikmanaj@hotmail.com>
2023-07-27 00:19:22 +0200

    Fixed error in countries-dropdown

--- dddSample/dddSample/ClientApp/src/app/countries-dropdown/countries-dropdown.component.ts
+++ dddSample/dddSample/ClientApp/src/app/countries-dropdown/countries-dropdown.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { CountriesService } from '../services/countries.service';
+import { Country } from '../models/country';
 
 @Component({
   selector: 'app-countries-dropdown',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./countries-dropdown.component.css']
 })
 export class CountriesDropdownComponent implements OnInit {
+  countries: Country[] = [];
+  selectedCountryId: number | null = null;
 
-  constructor() { }
+  @Output() countrySelected: EventEmitter<number | null> = new EventEmitter<number | null>();
+
+  constructor(private countriesService: CountriesService) {}
 
   ngOnInit(): void {
+    this.loadCountries();
   }
 
+  loadCountries(): void {
+    this.countriesService.getCountries().subscribe((countries) => {
+      this.countries = countries;
+    });
+  }
+
+  onCountryChange(): void {
+    this.countrySelected.emit(this.selectedCountryId);
+  }
 }
