a7b8d1167a7fb53b4941c70efec4a5abc0e14a85
Lori <lorikmanaj@hotmail.com>
2023-08-01 01:33:35 +0200

    Changed add-hotel html and uncommented Program.cs

--- dddSample/dddSample/Program.cs
+++ dddSample/dddSample/Program.cs
@@ -81,26 +81,3 @@ app.MapControllerRoute(
 app.MapFallbackToFile("index.html");
 
 app.Run();
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//builder.Services.AddControllersWithViews();
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//}
-
-//app.UseStaticFiles();
-//app.UseRouting();
-
-
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller}/{action=Index}/{id?}");
-
-//app.MapFallbackToFile("index.html"); ;
-
-//app.Run();
